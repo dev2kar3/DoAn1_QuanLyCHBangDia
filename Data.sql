bb722734770ee10ba665d949807a3b35c4ea3c7d
ngdangtan <nguyendangtan3578@gmail.com>
2023-10-04 19:52:30 +0700

    update

--- Data.sql
+++ Data.sql
@@ -1,10 +1,11 @@
-create database QuanLyCuaHangBangDia
+﻿create database QuanLyCuaHangBangDia
 go
 
 use QuanLyCuaHangBangDia
 go
 
 -- Account
+-- MemberInfo
 -- CompactDisc
 -- Bill
 -- BillInfo
@@ -13,39 +14,65 @@ go
 
 create table Account
 (
-	id int identity primary key not null,
-	accountName nvarchar (100),
+	userName nvarchar(100) primary key,
+	userPassWord nvarchar(1000) not null default 0,
+	displayName nvarchar (100) not null default N'Totem',
+)
+go
+
+create table memberInfo
+(
+	id int identity primary key,
+	accountName nvarchar (100) not null,
 	gender nvarchar (5),
 	phoneNumber bigint,
 	userAddress nvarchar (300),
-	identify bigint,
+	identify bigint not null,
 )
 go
 
-create table CompactDisc
+create table CompactDisc -- Thông tin đĩa
 (	
-	id int identity primary key not null,
-	cdName nvarchar (100),
-	genre nvarchar (30),
-	cdStatus nvarchar (30),
+	id int identity primary key,
+	cdName nvarchar (100) not null,
+	rentalPrice float not null default 0,
+	quantity int not null, -- Max 5 đĩa mỗi loại
+	remainCd int not null,
+	cdStatus nvarchar (30) not null default N'Có Sẵn',		-- Có Sẵn || Đã Thuê
 	productionComp nvarchar (300),
-	note nvarchar (300),
+	note nvarchar (300),									-- Mới || Cũ || Hỏng
+	idCategory int not null,
+
+	foreign key (idCategory) references dbo.CompactDiscCategory(id)
 )
 go
 
-create table Bill
+create table CompactDiscCategory
 (
-	id int identity primary key not null,
-	cdName nvarchar (100),
-	genre nvarchar (30),
-	cdStatus nvarchar (30),
-	quantity int,
-	borrowDay int,
+	id int identity primary key,
+	cateName nvarchar (100) not null,
 )
 go
 
-create table BillInfo
-()
+create table Bill -- Thông tin 1 loại đĩa được thuê
+(
+	id int identity primary key,
+	idCd int not null,
+	cdStatus nvarchar (30),									-- Khi đĩa được trả lại bị cũ hỏng hóc cần tăng tiền
+	quantity int not null,
+	borrowDay int not null,
+
+	foreign key (idCd) references dbo.CompactDisc(id)
+)
+go
+
+create table BillInfo -- Thông tin đơn tất cả đĩa của khách hàng đã thuê
+(
+	id int identity primary key,
+	idBill int not null,
+
+	foreign key (idBill) references dbo.Bill(id)
+)
 go
 
 
